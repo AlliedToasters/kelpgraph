c143350d5927c41ac4fdf6a3e3e6cd01965c6365
Michael Klear <michael.klear@nike.com>
2021-08-18 11:37:38 -0700

    modify for kelp

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,31 +5,33 @@ import {
   OwnershipTransferred,
   Transfer
 } from "../generated/kelptoken/kelptoken"
-import { ExampleEntity } from "../generated/schema"
+import { KelpHolder, Trans } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
+export function handleTransfer(event: Transfer): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let fromAccount = KelpHolder.load(event.transaction.from.toHex())
+  if (fromAccount == null) {
+    fromAccount = new KelpHolder(event.transaction.from.toHex())
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  let toAccount = KelpHolder.load(event.transaction.to.toHex())
+  if (toAccount == null) {
+    toAccount = new KelpHolder(event.transaction.to.toHex())
+  }
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
+  let transfer = new Trans(
+    event.transaction.hash.toHex(),
+    fromAccount,
+    toAccount,
+    event.transaction.value
+    )
+  
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  fromAccount.save()
+  toAccount.save()
+  transfer.save()
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -64,5 +66,3 @@ export function handleApproval(event: Approval): void {
 }
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleTransfer(event: Transfer): void {}
